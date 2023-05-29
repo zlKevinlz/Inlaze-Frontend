3d9939739e63b90d45f767e79dd431045fc24090
Kevin Alexander Rodriguez Bedoya <kevinalexander.rodriguez@utp.edu.co>
2023-05-29 18:13:27 -0500

    proyecto creado

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ItemComponent } from './components/item/item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    HeaderComponent,
+    ItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
